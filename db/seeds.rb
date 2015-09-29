1d67ecf4ba2251590cea746df00d98518f056fc1
gaogao <gaoyh@cimu.com.cn>
2015-09-29 18:20:11 +0800

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,17 +96,89 @@ stores = [
     distance: 700,
     logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/老街.jpg"
   },
+  {
+    name: "知味观",
+    kind: type[1],
+    address: "西湖区外东山弄86号",
+    price: 27,
+    distance: 1800,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/知味观.jpg"
+  },
+  {
+    name: "楼外楼",
+    kind: type[1],
+    address: "西湖区孤山路30号",
+    price: 149,
+    distance: 3300,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/楼外楼.jpg"
+  },
+  {
+    name: "阿三烧烤",
+    kind: type[5],
+    address: "文一路万塘录交叉路口391号",
+    price: 40,
+    distance: 1400,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/阿三烧烤.jpg"
+  },
+  {
+    name: "王记金图门窜烤",
+    kind: type[5],
+    address: "西湖区丰潭路282号",
+    price: 53,
+    distance: 1500,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/王记金图门窜烤.jpg"
+  },
+  {
+    name: "世纪联华超市",
+    kind: type[0],
+    address: "杭州市西湖区文二西路2号",
+    price: 97,
+    distance: 1500,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/世纪联华超市.jpg"
+  },
+  {
+    name: "小林刺身",
+    kind: type[6],
+    address: "杭州市西湖区西溪路418号",
+    price: 25,
+    distance: 800,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/小林刺身.jpg"
+  },
+  {
+    name: "舟山海鲜面",
+    kind: type[4],
+    address: "杭州市西湖区塘苗路102号",
+    price: 25,
+    distance: 200,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/舟山海鲜面.jpg"
+  },
+  {
+    name: "麦当劳",
+    kind: type[3],
+    address: "杭州市西湖区文三路478号华星时代广场",
+    price: 15,
+    distance: 600,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/麦当劳.jpg"
+  },
+  {
+    name: "百年锅贴",
+    kind: type[3],
+    address: "杭州市西湖区万塘路黄龙时代广场1楼",
+    price: 20,
+    distance: 400,
+    logo: "http://7xn4n5.com1.z0.glb.clouddn.com/logo/百年锅贴.jpg"
+  },
 ]
 
 i = 0
-11.times do
+20.times do
   store = Store.new
   store.name = stores[i][:name]
   store.kind = stores[i][:kind]
   store.price = stores[i][:price]
   store.distance = stores[i][:distance]
   store.address = stores[i][:address]
-  store.level = Faker::Number.between(1,5)
+  store.level = Faker::Number.between(4,5)
   store.logo = stores[i][:logo]
   store.creater_id = 0
   store.save
